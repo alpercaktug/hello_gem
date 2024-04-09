6d7b8c8c83c07834ee722c9f1cd8abd7454e63db
Alper Çaktuğ <alpercktg@gmail.com>
2024-04-09 10:38:53 +0300

    test webhook

--- lib/hello_gem.rb
+++ lib/hello_gem.rb
@@ -5,4 +5,8 @@ require_relative "hello_gem/version"
 module HelloGem
   class Error < StandardError; end
   # Your code goes here...
+  def self.greet(name)
+    puts "Hello, #{name}! I'm Ruby!"
+  end
+
 end
